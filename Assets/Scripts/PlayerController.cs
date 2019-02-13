7d0fa525a43d850798018e1d962ea60b606a3ff1
Jon Mora ENTI <freakill@gmail.com>
2019-02-13 19:58:03 +0100

    Add control logic & folder housekeeping

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1);
+        }
+    }
+}
